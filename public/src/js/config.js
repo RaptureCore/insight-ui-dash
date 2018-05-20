f51ecdab740ae097f898903592888602fb948d21
RaptureDev <35347205+RaptureCore@users.noreply.github.com>
2018-05-20 08:19:17 -0500

    Rapify

--- public/src/js/config.js
+++ public/src/js/config.js
@@ -5,7 +5,7 @@ angular.module('insight').config(function($routeProvider) {
   $routeProvider.
     when('/block/:blockHash', {
       templateUrl: 'views/block.html',
-      title: 'Dash Block '
+      title: 'Rapture Block '
     }).
     when('/block-index/:blockHeight', {
       controller: 'BlocksController',
@@ -17,7 +17,7 @@ angular.module('insight').config(function($routeProvider) {
     }).
     when('/tx/:txId/:v_type?/:v_index?', {
       templateUrl: 'views/transaction.html',
-      title: 'Dash Transaction '
+      title: 'Rapture Transaction '
     }).
     when('/', {
       templateUrl: 'views/index.html',
@@ -25,15 +25,15 @@ angular.module('insight').config(function($routeProvider) {
     }).
     when('/blocks', {
       templateUrl: 'views/block_list.html',
-      title: 'Dash Blocks solved Today'
+      title: 'Rapture Blocks solved Today'
     }).
     when('/blocks-date/:blockDate/:startTimestamp?', {
       templateUrl: 'views/block_list.html',
-      title: 'Dash Blocks solved '
+      title: 'Rapture Blocks solved '
     }).
     when('/address/:addrStr', {
       templateUrl: 'views/address.html',
-      title: 'Dash Address '
+      title: 'Rapture Address '
     }).
     when('/status', {
       templateUrl: 'views/status.html',
